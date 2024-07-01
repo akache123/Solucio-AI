a67027c82be92fb5f73090dabeb0f3249531eefc
Shreyas <slad0716@gmail.com>
2024-07-01 14:16:39 -0700

    feat: add layout & new dashboard page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 // src/pages/_app.js
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Layout from "./layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ClerkProvider>
   );
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,8 @@
-export default function LandingPage() {
+export default function Dashboard() {
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Landing Page
+        Dashboard
       </h1>
     </div>
   );
