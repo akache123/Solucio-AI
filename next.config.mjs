1188b700efcd192e39aee825f5989c787941572d
Shreyas <slad0716@gmail.com>
2024-07-01 12:51:23 -0700

    fix: remove root page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/onboardingpage',
-          permanent: false, // Set this to `true` if you want this to be a permanent redirect (HTTP 301)
-        },
-      ];
-    },
+    // async redirects() {
+    //   return [
+    //     {
+    //       source: '/',
+    //       destination: '/onboardingpage',
+    //       permanent: false, // Set this to `true` if you want this to be a permanent redirect (HTTP 301)
+    //     },
+    //   ];
+    // },
   };
   
   export default nextConfig;
