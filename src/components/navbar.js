605564cb05a12c49d7c6d37eda746830f5cf51d9
Shreyas <slad0716@gmail.com>
2024-07-01 14:05:45 -0700

    feat: add navbar avatar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,25 +3,38 @@ import {
   SignOutButton,
   SignedIn,
   SignedOut,
+  useUser,
 } from "@clerk/nextjs";
 
 import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export function Navbar() {
+  const { isLoaded, isSignedIn, user } = useUser();
+
   return (
     <div className="w-full border-b px-6 py-3">
       <div className="flex flex-row justify-between items-center">
         <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
-          Solucio
+          Solucio AI
         </h3>
-        <SignedOut>
-          <Button variant={"secondary"}>
-            <SignInButton />
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
+
+        {isSignedIn ? (
+          <SignedIn>
+            <Avatar>
+              <AvatarImage src={user.imageUrl} />
+              <AvatarFallback>
+                {user.firstName[0] + user.lastName[0]}
+              </AvatarFallback>
+            </Avatar>
+          </SignedIn>
+        ) : (
+          <SignedOut>
+            <Button variant={"secondary"}>
+              <SignInButton />
+            </Button>
+          </SignedOut>
+        )}
       </div>
     </div>
   );
