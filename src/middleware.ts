7278f3244177431b6f84d0c90ea50147b23c0303
Shreyas <slad0716@gmail.com>
2024-07-01 12:59:49 -0700

    refactor: trim react & clerk boilerplate

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Define route matchers based on your application's routing structure
 const isPublicRoute = createRouteMatcher([
-  '/',                 // Home page
-  '/sign-in(.*)',      // Matches any sub-route under /sign-in
-  '/sign-up(.*)'       // Matches any sub-route under /sign-up
+  "/", // Home page
+  "/sign-in(.*)", // Matches any sub-route under /sign-in
+  "/sign-up(.*)", // Matches any sub-route under /sign-up
 ]);
 
 // Configuration for Clerk middleware
@@ -12,25 +12,25 @@ const clerkOptions = {
   publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   secretKey: process.env.CLERK_SECRET_KEY,
   domain: process.env.CLERK_API_URL,
-  signInUrl: "/sign-in",
-  signUpUrl: "/sign-up",
+  // signInUrl: "/sign-in",
+  // signUpUrl: "/sign-up",
   afterSignInUrl: "/dashboard",
   afterSignUpUrl: "/dashboard",
-  isSatellite: false
+  isSatellite: false,
 };
 
 // Clerk middleware function
 export default clerkMiddleware((auth, request, event) => {
   if (!isPublicRoute(request)) {
-    auth().protect();  // Protect all routes that are not explicitly public
+    auth().protect(); // Protect all routes that are not explicitly public
   }
 }, clerkOptions);
 
 // Configuration for Next.js middleware behavior
 export const config = {
   matcher: [
-    '/((?!.+\.[\w]+$|_next).)*',  // Matches all routes except for files and _next specific routes
-    '/',                          // Home page
-    '/(api|trpc)(.*)'             // API routes and potentially tRPC routes
+    "/((?!.+.[w]+$|_next).)*", // Matches all routes except for files and _next specific routes
+    "/", // Home page
+    "/(api|trpc)(.*)", // API routes and potentially tRPC routes
   ],
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,10 @@
 // src/pages/_app.js
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  const [clerkPublishableKey, setClerkPublishableKey] = useState(null);
-
-  useEffect(() => {
-    const key = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-    if (key) {
-      setClerkPublishableKey(key);
-    } else {
-      console.error('Missing publishable key for Clerk. Check your .env.local file.');
-    }
-  }, []);
-
-  if (!clerkPublishableKey) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <ClerkProvider publishableKey={clerkPublishableKey}>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
