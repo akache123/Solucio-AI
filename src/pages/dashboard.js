b870db9422e38f604e4f57f09c8547aba522ba43
meghagl17 <megha.agl17@gmail.com>
2024-07-18 18:41:39 -0700

    updating some styling for the premium recommendation

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -186,29 +186,61 @@ export default function Dashboard() {
           </div>
       ) : (
         <div>
-          <div className="scroll-m-20 pb-2 text-4xl font-semibold tracking-tight first:mt-0">Your Recommendations</div>
-          <CardHeader>
+          <div className="scroll-m-15 pb-2 text-4xl font-semibold tracking-tight first:mt-0">Your Recommendations</div>
+          {premiumRecommendation.topRestuarants}
           {premiumRecommendation.length > 0 && (
-            <div className="flex">
-              <CardTitle style={{ marginRight: '10px' }}>{premiumRecommendation[0]?.name}</CardTitle>
-              <Dialog>
-                <DialogTrigger><Maximize2 /></DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>more info!</DialogTitle>
-                    <DialogDescription>
-                      more information will come soon
-                    </DialogDescription>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
-            </div>
-          )}
-          <CardDescription>{premiumRecommendation[0]?.name}</CardDescription>
+  <div className="relative mb-4 w-2/3 mx-auto">
+    {/* Badge positioned absolutely */}
+    <Badge 
+      style={{ marginRight: '10px' }} 
+      variant="outline" 
+      className="absolute top-[-1.5rem] left-1/2 transform -translate-x-1/2 z-10"
+    >
+      Premium Recommendation
+    </Badge>
+    {/* Card content */}
+    <div className="flex border rounded-md items-center">
+      <div className="w-1/3 h-48 bg-gray-300 rounded-l-md flex items-center pl-4">
+        <span className="text-gray-500">Image Placeholder</span>
+      </div>
+      <div className="p-4 flex flex-col justify-between w-2/3">
+        <CardHeader>
+          <div className="flex">
+            <CardTitle style={{ marginRight: '10px' }}>{premiumRecommendation[0].name}</CardTitle>
+            <Dialog>
+              <DialogTrigger><Maximize2 /></DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>{premiumRecommendation[0].name}</DialogTitle>
+                  <DialogDescription>
+                    {premiumRecommendation[0].cuisine}
+                  </DialogDescription>
+                </DialogHeader>
+                <div>
+                  address: {premiumRecommendation[0].address}<br />
+                  calories: {premiumRecommendation[0].calories}<br />
+                  price: {premiumRecommendation[0].price}<br />
+                  website: {premiumRecommendation[0].website}
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <CardDescription>{premiumRecommendation[0].cuisine}</CardDescription>
         </CardHeader>
         <CardContent>
           <p>description</p>
         </CardContent>
+        <hr className="my-2 border-gray-300" />
+        <CardFooter>
+          {premiumRecommendation[0].dietLabels.map((tag) => (
+            <Badge key={tag} style={{ marginRight: '10px' }} variant="outline">{tag}</Badge>
+          ))}
+        </CardFooter>
+      </div>
+    </div>
+  </div>
+)}
+
 
           <div className="flex items-center justify-center min-h-screen">
             <ScrollArea className="w-[60%] rounded-md border">
